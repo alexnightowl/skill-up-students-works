fcdb300cc882f4af08eab0ad54efac8a9ec62f85
kondratiukandrii <90239424+kondratiukandrii@users.noreply.github.com>
2021-10-25 21:02:38 +0300

    homework-lesson

---
+++ Kondratyuk_Andrey/Homework_js-6/maib.js
@@ -0,0 +1,6 @@
+const array  = [1,2,3,5,,66,7,,7]
+const lict = document.querySelector('.empty');
+
+array.map(function(namber){
+    list.appendChild (document.createElement('li').innerHTML `$(namber)`
+})
