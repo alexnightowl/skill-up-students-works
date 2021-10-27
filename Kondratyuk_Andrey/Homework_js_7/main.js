9220f39130ae69020d65b309454df94357d16629
kondratiukandrii <90239424+kondratiukandrii@users.noreply.github.com>
2021-10-27 20:58:57 +0300

    homework_js_7

--- Kondratyuk_Andrey/Homework_js_7/main.js
+++ Kondratyuk_Andrey/Homework_js_7/main.js
@@ -1,1 +1,33 @@
-    
+function showAlert(mesege){
+    alert (mesege)
+}
+
+const textBlock =document.querySelector('.js-textRebock')
+
+textBlock.onclick =function( ) {
+    console.log(textBlock.innerTEXT)
+}
+
+const textField =document.querySelector('.js-textRebock')
+
+textField.addEventListener('click',function name(params) {
+    textField.style.backgroundColor =("red")
+})
+
+textField.addEventListener('input',function name(event) {
+    console.log(this)
+    textField.style.backgroundColor ="elou"
+})
+
+textField.addEventListener('blur',function name(params) {
+    textField.style.backgroundColor =("red")
+})
+
+const button =document.getElementById('btn')
+    button.addEventListener('click',function (event) {
+        let.mesege =this,this.parentElement.value;
+        console,log(event.target.value)
+        console.log(mesege);
+    })
+
+
