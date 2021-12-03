a16c4085d395f4821c5e4ddf0dabd690e1214de8
Anton <deguz@mail.ru>
2021-12-04 00:01:40 +0300

    main page visual improvements and starter animation adaptation added

--- Dektyarev_Anton/second.js
+++ Dektyarev_Anton/second.js
@@ -1,19 +1,37 @@
 let currentWindowHeight = window.innerHeight;
+let currentWindowWidth = window.innerWidth;
+let futterJumpValue;
+let navJumpValue;
 
-let futterJumpValue = currentWindowHeight * -0.44 - 20 + "px";
-let navJumpValue = currentWindowHeight * 0.06 + 32 + "px";
+if (currentWindowWidth > 768) {
+  futterJumpValue = currentWindowHeight * -0.5 + 20 + 8 + "px";
+  navJumpValue = currentWindowHeight * 0.5 - 244 + "px";
+} else {
+  futterJumpValue = currentWindowHeight * -0.5 + 20 + 64 + 8 + "px";
+  navJumpValue = currentWindowHeight * 0.5 - 244 + "px";
+}
 
 document.documentElement.style.setProperty(
   "--footterjumpvalue",
   futterJumpValue
 );
 document.documentElement.style.setProperty("--navjumpvalue", navJumpValue);
+console.log(futterJumpValue);
+console.log(navJumpValue);
 
 window.addEventListener("resize", function vpchupdate() {
   let currentWindowHeight = window.innerHeight;
+  let currentWindowWidth = window.innerWidth;
+  let futterJumpValue;
+  let navJumpValue;
 
-  let futterJumpValue = currentWindowHeight * -0.44 - 20 + "px";
-  let navJumpValue = currentWindowHeight * 0.06 + 32 + "px";
+  if (currentWindowWidth > 768) {
+    futterJumpValue = currentWindowHeight * -0.5 + 20 + 8 + "px";
+    navJumpValue = currentWindowHeight * 0.5 - 244 + "px";
+  } else {
+    futterJumpValue = currentWindowHeight * -0.5 + 20 + 64 + 8 + "px";
+    navJumpValue = currentWindowHeight * 0.5 - 244 + "px";
+  }
 
   document.documentElement.style.setProperty(
     "--footterjumpvalue",
